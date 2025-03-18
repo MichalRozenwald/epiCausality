3d5b27f9f9cfdbfb52d25da5cee8964842124dd6
MichalRozenwald <michal.rozenwald@gmail.com>
2025-03-18 06:06:21 -0700

    Create VCF files to look for Haplotype phasing of our T cells data as the sample from 1000 Genomes didn't have SNPs in our target region

--- epiCode/haplotype_phasing/vcf_varience_calling_T_cells_CRISPROff_deeper.bash
+++ epiCode/haplotype_phasing/vcf_varience_calling_T_cells_CRISPROff_deeper.bash
@@ -25,6 +25,28 @@ bcftools index forced_variants.chr1_206560169_206614236.vcf.gz
 bcftools view -r chr1:206560169-206614236 forced_variants.chr1_206560169_206614236.vcf.gz | head -20
 
 
+top - 05:42:23 up 19 days, 10:43,  1 user,  load average: 1.17, 1.02, 0.77
+Tasks: 442 total,   2 running, 440 sleeping,   0 stopped,   0 zombie
+%Cpu(s):  4.2 us,  0.1 sy,  0.0 ni, 95.7 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+MiB Mem : 128554.4 total,  47447.2 free,   2883.7 used,  78223.4 buff/cache
+MiB Swap:   2048.0 total,   1036.6 free,   1011.4 used. 124437.2 avail Mem 
 
+    PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND                                                        
+1593328 michalu+  20   0  362344 355044   5184 R 100.0   0.3   9:33.73 bcftools                                                       
+ 421741 minknow   35  15 2374072   7216   5172 S   1.0   0.0 490:11.37 control_main                                                   
+ 421742 minknow   35  15 6952512   6612   4812 S   0.7   0.0   1433:39 control_main                                                   
+    955 minknow   20   0 9962
+top - 05:42:23 up 19 days, 10:43,  1 user,  load average: 1.17, 1.02, 0.77
+Tasks: 442 total,   2 running, 440 sleeping,   0 stopped,   0 zombie
+%Cpu(s):  4.2 us,  0.1 sy,  0.0 ni, 95.7 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+MiB Mem : 128554.4 total,  47447.2 free,   2883.7 used,  78223.4 buff/cache
+MiB Swap:   2048.0 total,   1036.6 free,   1011.4 used. 124437.2 avail Mem 
 
+    PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND                                                        
+1593328 michalu+  20   0  362344 355044   5184 R 100.0   0.3   9:33.73 bcftools                                                       
+ 421741 minknow   35  15 2374072   7216   5172 S   1.0   0.0 490:11.37 control_main                                                   
+ 421742 minknow   35  15 6952512   6612   4812 S   0.7   0.0   1433:39 control_main                                                   
+    955 minknow   20   0 9962
+
+    
 # Look at a broader set: chr1:206265172-206698324
